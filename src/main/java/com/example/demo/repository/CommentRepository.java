36c14308522345c1d918d322d13b388a96509277
jiegegege <741993444@qq.com>
2018-12-21 11:17:23 +0800

    在User.Movie.Comment增加接口

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Comment;
+import com.example.demo.entity.Movie;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>{
-
+	//查看某部电影所有评论
+	public List<Comment> findByMovie(Movie movie);
+	//通过评论时间排序
+	public List<Comment> findByMovieOrderByDate(Movie movie);
+	
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	//通过手机号查找用户
+	public User findByPhone(String phone);
+	//用户登录
+	public User findByPhoneAndPassword(String phone,String password);
 }
