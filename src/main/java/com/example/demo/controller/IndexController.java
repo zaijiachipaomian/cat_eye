c16bdf25a897c02312924e9cb4939b3319daa7d0
JessieChn <183398897@qq.com>
2018-12-26 15:49:31 +0800

    添加了注意的跳转链接

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -5,23 +5,31 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.TodayBoxOffice;
 import com.example.demo.service.BoxOfficeService;
 
-@RestController
+@Controller
 public class IndexController {
 	
 	@Autowired
 	BoxOfficeService boxOfficeService;
 	
-	@GetMapping("/")
+	@ResponseBody
+	@GetMapping("/index_data/todayBoxOffice")
 	public Map<String, Object> getIndexInfor() {
 		Map<String , Object> map = new HashMap<String , Object>();
 		List<TodayBoxOffice> todayBoxOffice = boxOfficeService.getTodayBoxOffice();
 		map.put("todayBoxOffice", todayBoxOffice);
 		return map;
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
 	}
 }
