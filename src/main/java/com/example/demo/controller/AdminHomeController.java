a5973ab799010654fef11e47aadf6e4df3e3beae
unknown <cseyhsu>
2018-12-27 19:52:45 +0800

    增加管理员登陆ç的功能å

--- src/main/java/com/example/demo/controller/AdminHomeController.java
+++ src/main/java/com/example/demo/controller/AdminHomeController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import com.example.demo.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,29 +29,71 @@ public class AdminHomeController {
         return "admin/index";
     }
 
+    @Autowired
+    UserService userService ;
     // 管理员登陆页面
     @ResponseBody
     @PostMapping("/login")
-    public Object login(HttpServletRequest request, String name, String pwd) {
+    public Object login(HttpServletRequest request, String phone, String pwd) {
         //
-        Admin admin = new Admin();
-        admin.name = name;
-        admin.pwd = pwd;
-        request.setAttribute(ADMIN, admin);
+        System.out.println("管理员登陆 "+phone+ " pwd "+ pwd );
 
+        Admin admin = new Admin();
         Response response = new Response();
+        admin.phone = phone;
+        admin.pwd = pwd;
+        // 排除空值
+        // 403
+        if (admin.phone == null || pwd == null || phone.equals("") || pwd.equals("")){
+            response.code = 403;
+            response.data = "forbidden";
+            return response;
+        }
+
+
+        User login = userService.login(phone, pwd);
+        if (login == null){
+            response.code = 403;
+            response.data = "手机号码或者密码错误,请重新登陆";
+            return response;
+        }
+        request.getSession().setAttribute(ADMIN,admin);
+
+
         response.code = HTTP_OK;
         response.data = "ok";
         return response;
     }
 
-    static class Admin {
-        String name;
+     class Admin {
+        String phone;
         String pwd;
     }
 
-    static class Response {
+     public class Response {
         int code; // 状态码
         Object data; // 返回信息
-    }
+
+         public int getCode() {
+             return code;
+         }
+
+         public void setCode(int code) {
+             this.code = code;
+         }
+
+         public Object getData() {
+             return data;
+         }
+
+         public void setData(Object data) {
+             this.data = data;
+         }
+
+         @Override
+         public String toString() {
+             System.out.println("hello world toString ...... "+JSON.toJSONString(this));
+             return JSON.toJSONString(this);
+         }
+     }
 }
